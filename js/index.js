4846b59f5479ac58ab9234ecf1772a94e45d9f38
anjaligm <vignesh@vigneshs-MacBook-Air.local>
2016-11-27 00:40:51 +0530

    slider changed

--- js/index.js
+++ js/index.js
@@ -1,22 +1,4 @@
-    //hero text fade flexslider
-    $(window).load(function() {
-        $('.flexslider').flexslider({
-            controlNav: false,
-            directionNav: false,
-            slideshowSpeed: 4000
-        });
-    });
 
-    //home slider
-    $(window).load(function() {
-        $('.main-slider').flexslider({
-            controlNav: false,
-            directionNav: true,
-            slideshowSpeed: 4000,
-            prevText: "<i class='ion-chevron-left'></i>",
-            nextText: "<i class='ion-chevron-right'></i>"
-        });
-    });
 
 
     $(document).ready(function() {
@@ -69,59 +51,3 @@
         });
 
     });
-
-    /*!
- * Start Bootstrap - Creative v3.3.7+1 (http://startbootstrap.com/template-overviews/creative)
- * Copyright 2013-2016 Start Bootstrap
- * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap/blob/gh-pages/LICENSE)
- */
-
-! function(a) {
-    "use strict";
-    a("a.page-scroll").bind("click", function(e) {
-        var l=a(this);
-        a("html, body").stop().animate( {
-            scrollTop: a(l.attr("href")).offset().top - 50
-        }
-        , 1250, "easeInOutExpo"), e.preventDefault()
-    }
-    ),
-    a("body").scrollspy( {
-        target: ".navbar-fixed-top", offset: 51
-    }
-    ),
-    a(".navbar-collapse ul li a").click(function() {
-        a(".navbar-toggle:visible").click()
-    }
-    ),
-    a("#mainNav").affix( {
-        offset: {
-            top: 100
-        }
-    }
-    ),
-    window.sr=ScrollReveal(),
-    sr.reveal(".sr-icons", {
-        duration: 600, scale: .3, distance: "0px"
-    }
-    , 200),
-    sr.reveal(".sr-button", {
-        duration: 1e3, delay: 200
-    }
-    ),
-    sr.reveal(".sr-contact", {
-        duration: 600, scale: .3, distance: "0px"
-    }
-    , 300),
-    a(".popup-gallery").magnificPopup( {
-        delegate: "a", type: "image", tLoading: "Loading image #%curr%...", mainClass: "mfp-img-mobile", gallery: {
-            enabled: !0, navigateByImgClick: !0, preload: [0, 1]
-        }
-        , image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-        }
-    }
-    )
-}
-
-(jQuery);
